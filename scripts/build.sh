05fac41e1a1d17a67948388d68fea8947e114395
thebestlearner-ric <watto1990@gmail.com>
2023-09-30 12:13:36 +0800

    refactor

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,6 @@
 
 # This will be the name of the resulting Docker image
 # This values could come from the GoCD environment
-cd air-backend-repo
 DOCKER_REPO=learningric/air_artifact
 TAG_NAME=$(git rev-parse --short=8 HEAD)
 ENV=backend
